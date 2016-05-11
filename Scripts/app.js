8b82205c8f746d326308a672565af9c74face754
erqe <qrqr@yge.shdg>
2016-05-11 17:05:53 -0400

    Initial Commit

---
+++ Scripts/app.js
@@ -0,0 +1,13 @@
+// custom JavaScript
+//selg-executing anoymous function
+
+//IIFE- Immediately Invoked Function Expression
+var myname="Satnam Singh";
+(function () {
+	
+console.log("App Started....");
+
+var number=0;
+number =Math.floor((Math.random()*10)+1);
+	console.log(number);
+})();
